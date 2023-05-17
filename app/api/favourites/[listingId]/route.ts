d37dfb728d81833c1cc3bda3fd8a2648b77c1fdb
AsijitM <asijit00@gmail.com>
2023-05-17 15:58:15 +0530

    routes for favorite feature

---
+++ app/api/favourites/[listingId]/route.ts
@@ -0,0 +1,64 @@
+import { NextResponse } from 'next/server';
+
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import prisma from '@/app/libs/prismadb';
+
+interface Iparams {
+  listingId?: string;
+}
+
+export async function POST(request: Request, { params }: { params: Iparams }) {
+  const CurrentUser = await getCurrentUser();
+
+  if (!CurrentUser) return NextResponse.error();
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  let favoriteIds = [...(CurrentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: CurrentUser.id,
+    },
+    data: {
+      favoriteIds: favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Iparams }
+) {
+  const CurrentUser = await getCurrentUser();
+
+  if (!CurrentUser) return NextResponse.error();
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  let favoriteIds = [...(CurrentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+  //updating in DB
+  const user = await prisma.user.update({
+    where: {
+      id: CurrentUser.id,
+    },
+    data: {
+      favoriteIds: favoriteIds,
+    },
+  });
+  return NextResponse.json(user);
+}
