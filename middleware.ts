1964aa0f389d9dfd39129edb12410e595016562c
AsijitM <asijit00@gmail.com>
2023-05-23 02:21:27 +0530

    protected Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/trips', '/reservations', '/properties', '/favorites'],
+    matcher: ['/trips',
+        '/reservations',
+        '/properties',
+        '/favorites'
+    ],
 };
